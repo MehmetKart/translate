7b8a00867025d9e0a3afbcab2ec84287f646a804
mehmet.kart <mehmet.kart@mehmetk-MacBook-Pro.local>
2022-04-10 01:33:04 +0300

    feat (voice): add voice translate

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import {CustomInput} from './styles';
 import PropTypes from 'prop-types';
 
-const Input = ({placeholder, onChangeText}) => {
-  return <CustomInput placeholder={placeholder} onChangeText={onChangeText} />;
+const Input = ({placeholder, onChangeText, value}) => {
+  return (
+    <CustomInput
+      placeholder={placeholder}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
 };
 
 Input.propTypes = {
   placeholder: PropTypes.string,
   onChangeText: PropTypes.func,
+  value: PropTypes.string,
 };
 
 export default Input;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,14 +2,22 @@ import React, {useEffect, useState} from 'react';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import Text from '../../components/Text';
-import {Container} from './styles';
+import {Container, RoundedButton} from './styles';
 import {translateTo} from '../../services';
+import Voice from '@react-native-voice/voice';
 
 const Home = () => {
   const [query, setQuery] = useState('');
   const [to, setTo] = useState('en');
   const [from, setFrom] = useState('tr');
   const [translated, setTranslated] = useState('');
+  const [recordStatus, setRecordStatus] = useState('Başlat');
+
+  const onSpeechStartHandler = () => {};
+  const onSpeechEndHandler = () => {};
+  const onSpeechResultsHandler = e => {
+    setQuery(e.value[0]);
+  };
 
   const [placeholder, setPlaceholder] = useState({
     toPlaceholder: 'İngilizce',
@@ -17,15 +25,41 @@ const Home = () => {
   });
 
   useEffect(() => {
-    translate();
-  }, []);
-
-  useEffect(() => {
     setPlaceholder({
       toPlaceholder: to === 'en' ? 'İngilizce' : 'Türkçe',
       fromPlaceholder: from === 'en' ? 'İngilizce' : 'Türkçe',
     });
   }, [to, from]);
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStartHandler;
+    Voice.onSpeechEnd = onSpeechEndHandler;
+    Voice.onSpeechResults = onSpeechResultsHandler;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startRecording = async () => {
+    let languageCode = to === 'en' ? 'en-US' : 'tr';
+    try {
+      setRecordStatus('Durdur');
+      await Voice.start(languageCode);
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      translate();
+      setRecordStatus('Başlat');
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
 
   const translate = async () => {
     await translateTo(query, to, from)
@@ -36,6 +70,8 @@ const Home = () => {
   };
 
   const replaceTranslate = () => {
+    setQuery('');
+    setTranslated('');
     setTo(to === 'en' ? 'tr' : 'en');
     setFrom(from === 'en' ? 'tr' : 'en');
   };
@@ -53,6 +89,12 @@ const Home = () => {
       {!!translated && <Text text={translated} size="16px" />}
 
       <Button text="Çevir" bgColor="#45b6fe" onClick={translate} />
+
+      <Button
+        text={recordStatus}
+        onClick={recordStatus === 'Başlat' ? startRecording : stopRecording}
+        bgColor={recordStatus === 'Başlat' ? '#26580f' : '#bb0a1e'}
+      />
 
       <Button text="Dili Değiştir" onClick={replaceTranslate} />
     </Container>
