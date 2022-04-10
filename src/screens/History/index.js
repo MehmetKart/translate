b0a8f08c4ffb28836b9ba0133df7374b431150dd
mehmet.kart <mehmet.kart@mehmetk-MacBook-Pro.local>
2022-04-10 17:34:01 +0300

    feat (history): create history list

--- src/screens/History/index.js
+++ src/screens/History/index.js
@@ -1,11 +1,37 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Container, ListContainer, Text} from './styles';
+import {FlatList} from 'react-native';
+import {getData} from '../../helpers/index';
 
 const History = () => {
+  const [historyData, setHistoryData] = useState([]);
+
+  useEffect(() => {
+    getHistoryData();
+  }, []);
+
+  const getHistoryData = async () => {
+    const history = await getData();
+    setHistoryData(history);
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <ListContainer>
+        <Text> {item.query} </Text>
+        <Text> {item.translated} </Text>
+      </ListContainer>
+    );
+  };
+
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <Container>
+      <FlatList
+        data={historyData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </Container>
   );
 };
 
---
+++ src/screens/History/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+const ListContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-horizontal: 20px;
+`;
+
+const Text = styled.Text`
+  font-size: 14px;
+  color: black;
+  margin-vertical: 5px;
+`;
+
+export {Container, ListContainer, Text};
